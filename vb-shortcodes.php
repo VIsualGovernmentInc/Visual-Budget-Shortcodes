9a239d9a5af8a8211464bde61f63096975014425
Visual Government Inc <github@visgov.com>
2018-05-17 22:09:33 -0400

    Added div classes
    Added to the left, right and full divs the followign classes:
    vb-metric-block
    vb-vis-left
    vb-vis-right
    vb-vis-full

--- vb-shortcodes.php
+++ vb-shortcodes.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Visual Budget - Add-on shortcodes
- * @version 0.1
+ * @version 0.2 (added responsive widths)
  */
 /*
 Plugin Name: Visual Budget - Add-on shortcodes
 Plugin URI: http://visgov.com/
 Description: Composite shortcodes for visualizations.
 Author: Alan Jones
-Version: 0.1
+Version: 0.2
 Author URI: http://visgov.com/
 */
 
@@ -18,9 +18,10 @@ function vb_metric_block_func($atts ) {
 	'Total: [visualbudget data='.$a['data'].' vis=metric metric=datetotal]<br/>'.
 	'$ Growth: [visualbudget data='.$a['data'].' vis=metric metric=absgrowth]<br/>'.
 	'% Growth: [visualbudget data='.$a['data'].' vis=metric metric=percentgrowth]<br/>'.
-	'[visualbudget data='.$a['data'].' vis=metric metric=numyearsaveraged]-year average: [visualbudget data=expenses vis=metric metric=5yearaverage]<br/>'.
-	'Download this dataset [visualbudget data='.$a['data'].' vis=metric metric=download]';	
-	return '<div style="float:left; width:30%;">'.do_shortcode($ret).'</div>';
+	'[visualbudget data='.$a['data'].' vis=metric metric=numyearsaveraged]-year average: [visualbudget data='.$a['data'].' vis=metric metric=5yearaverage]<br/>'.
+	'Download this dataset [visualbudget data='.$a['data'].' vis=metric metric=download]';
+	// return '<div style="float:left; width:30%;">'.do_shortcode($ret).'</div>';
+	return '<div class="vb-metric-block">'.do_shortcode($ret).'</div>';
 }
 add_shortcode( 'vb_metric_block', 'vb_metric_block_func');
 
@@ -29,11 +30,13 @@ function vb_vis_left_func($atts) {
 		'data' => '',
 		'vis' => '',
 		'before' => '',
-		'after' => ''
+		'after' => '',
+		'smooth' => ''
 		), $atts );
-		$ret = '[visualbudget data='.$a['data'].' vis='.$a['vis'].']';
+		$ret = '[visualbudget data='.$a['data'].' vis='.$a['vis'].' smooth='.$a['smooth'].']';
 		$ret = $a['before'].$ret.$a['after'];
-		return '<div style="float:left; width:45%; clear:left;">'.do_shortcode($ret).'</div>';
+		// return '<div style="float:left; width:45%; clear:left;">'.do_shortcode($ret).'</div>';
+		return '<div class="vb-vis-left">'.do_shortcode($ret).'</div>';
 }
 add_shortcode('vb_vis_left','vb_vis_left_func');
 
@@ -43,11 +46,14 @@ function vb_vis_right_func($atts) {
 		'vis' => '',
 		'before' => '',
 		'after' => '',
-		'showmycontribution' => ''
+		'showmycontribution' => '',
+		'smooth' => ''
 		), $atts );
-		$ret = '[visualbudget data='.$a['data'].' vis='.$a['vis']. ' showmycontribution='.$a['showmycontribution'].']';
+		$ret = '[visualbudget data='.$a['data'].' vis='.$a['vis'].
+				 ' showmycontribution='.$a['showmycontribution'].' smooth='.$a['smooth'].']';
 		$ret = $a['before'].$ret.$a['after'];
-		return '<div style="float:right; clear:right; width:50%;">'.do_shortcode($ret).'</div>';
+		// return '<div style="float:right; clear:right; width:50%;">'.do_shortcode($ret).'</div>';
+		return '<div class="vb-vis-right">'.do_shortcode($ret).'</div>';
 }
 add_shortcode('vb_vis_right','vb_vis_right_func');
 
@@ -60,10 +66,18 @@ function vb_vis_full_func($atts) {
 		), $atts );
 		$ret = '[visualbudget data='.$a['data'].' vis='.$a['vis'].']';
 		$ret = $a['before'].$ret.$a['after'];
-		return '<div style="float:left; width:100%; clear:both;">'.do_shortcode($ret).'</div>';
+		// return '<div style="float:left; width:100%; clear:both;">'.do_shortcode($ret).'</div>';
+		return '<div class="vb-vis-full">'.do_shortcode($ret).'</div>';
 }
 add_shortcode('vb_vis_full','vb_vis_full_func');
 
+function vb_iframe_wrapper($atts) {
+	$a = shortcode_atts( array(
+		'page' => ''
+		), $atts );
+		return '<iframe src="'.$a['page'].'" width=100% height=600px>&nbsp;</iframe>';
+}
 
+add_shortcode('vb_iframe','vb_iframe_wrapper');
 
 ?>
